7003aa624786f8069e0ff237b53ebad456385462
Clay McLeod <Clay.McLeod@STJUDE.org>
2020-12-02 12:13:24 -0600

    feat: omnibar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,27 +2,41 @@ import React from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faArrowCircleLeft } from "@fortawesome/free-solid-svg-icons"
 import { faGithub } from "@fortawesome/free-brands-svg-icons"
+import ChildLogo from "../icons/stjudecloud-logo.svg"
 import SearchIcon from "../icons/search.svg"
 import CommandSymbol from "../icons/cmd.svg"
+import Menu from "../icons/menu.svg"
 
-const Navbar = ({ searchModal, onSearchClick }) => (
-  <nav className="sticky top-0 z-40 h-20 bg-white border-b shadow-sm border-solid border-coolGray-100 flex items-center justify-between w-full px-7">
-    <a
-      href="https://stjude.cloud"
-      className="flex flex-1 justify-left items-center cursor-pointer"
-    >
-      <FontAwesomeIcon
-        className="fill-current text-navy"
-        icon={faArrowCircleLeft}
-        style={{ width: "24px", height: "24px" }}
-      />
-      <span
-        className="m-2 font-bold font-sans text-navy select-none"
-        style={{ fontSize: "21px" }}
+const Navbar = ({ onSearchClick }) => (
+  <nav
+    className="sticky top-0 z-40 bg-white border-b shadow-sm border-solid border-coolGray-100 flex items-center justify-between w-full p-2 leading-4"
+    style={{ height: "65px" }}
+  >
+    <div className="flex flex-1 items-center justify-left ">
+      <a
+        href="https://stjude.cloud"
+        className="flex items-center cursor-pointer"
       >
-        stjude.cloud
-      </span>
-    </a>
+        <ChildLogo
+          className="fill-current text-navy border-navy border-r"
+          style={{ height: 40, width: 61 }}
+        />
+      </a>
+      <a href="/">
+        <span
+          className="text-navy"
+          style={{ paddingLeft: "10px", fontWeight: "800", fontSize: "20px" }}
+        >
+          St. Jude Cloud
+        </span>
+        <span
+          className="text-navy text-opacity-60"
+          style={{ paddingLeft: "8px", fontSize: "20px" }}
+        >
+          Docs
+        </span>
+      </a>
+    </div>
     <span
       className="hidden md:flex flex-1 justify-center items-center cursor-pointer"
       onClick={onSearchClick}
@@ -47,16 +61,29 @@ const Navbar = ({ searchModal, onSearchClick }) => (
         </span>
       </div>
     </span>
-    <a
-      href="https://github.com/stjudecloud/docs"
-      className="flex flex-1 items-center justify-end text-coolGray-400 cursor-pointer"
-    >
-      <FontAwesomeIcon
-        className="fill-current"
-        icon={faGithub}
-        style={{ width: "40px", height: "40px" }}
-      />
-    </a>
+    <div className="flex flex-1 items-center justify-end">
+      <div
+        className="flex items-center justify-center text-navy fill-current"
+        style={{ height: "40px", width: "40px", marginRight: "10px" }}
+      >
+        <a
+          href="https://github.com/stjudecloud/docs"
+          className="text-navy cursor-pointer"
+          style={{ paddingRight: "10px" }}
+        >
+          <FontAwesomeIcon
+            icon={faGithub}
+            style={{ width: "35px", height: "35px" }}
+          />
+        </a>
+      </div>
+      {/* <div
+        className="flex items-center justify-center text-navy fill-current"
+        style={{ height: "40px", width: "40px", marginRight: "10px" }}
+      >
+        <Menu style={{ width: "20px", height: "20px" }} />
+      </div> */}
+    </div>
   </nav>
 )
 
