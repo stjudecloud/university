875d8b577b9bcf4d5e91ace6245c12dae834a3af
Clay McLeod <Clay.McLeod@STJUDE.org>
2020-12-22 22:42:34 -0600

    fix: see if removing postcss purging removes the issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const colors = require("tailwindcss/colors")
 
 module.exports = {
-  purge: ["./src/**/*.html", "./src/**/*.js"],
+  // purge: ["./src/**/*.html", "./src/**/*.js"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
