53174f663f9fc55c928df9089556fc36eb2fa22c
Clay McLeod <Clay.McLeod@STJUDE.org>
2020-12-28 08:37:06 -0600

    fix(scroll): fixes scrolling to location hashes

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -7,3 +7,26 @@
 // You can delete this file if you're not using it
 
 import "./src/styles/global.scss"
+
+export function onRouteUpdate({ location }) {
+  return anchorScroll(location)
+}
+
+export function shouldUpdateScroll() {
+  return true
+}
+
+function anchorScroll(location, timeout = 250, offsetY = 65 + 10) {
+  if (location && location.hash) {
+    setTimeout(() => {
+      const item = document.querySelector(location.hash).offsetTop
+      window.scrollTo({
+        top: item - offsetY,
+        left: 0,
+        behavior: "smooth",
+      })
+    }, timeout)
+  }
+
+  return true
+}
