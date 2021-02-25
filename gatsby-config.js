6ea333c4cb746ed80847db684ed411f4aea92b3e
Arthur Chiao <arthur.chiao@stjude.org>
2021-02-25 11:20:05 -0600

    build: add gatsby-redirect-from plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -161,6 +161,8 @@ let plugins = [
     },
   },
   `gatsby-check-slugs`,
+  `gatsby-redirect-from`,
+  'gatsby-plugin-meta-redirect' // gatsby-plugin-meta-redirect must be last entry
 ]
 
 if (process.env.INTERCOM_APP_ID) {
